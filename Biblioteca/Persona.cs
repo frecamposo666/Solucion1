365349d227714b905f51d1fde8460bf19b0e601d
666 <campos.fm@gmail.com>
2019-03-27 12:28:22 -0300

    todo ok

---
+++ Biblioteca/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class Persona
+    {
+        public String Rut { get; set; }
+        public String Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public Persona()
+        {
+
+        }
+    }
+}
---
+++ BibliotecaControlador/DaoPersona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Biblioteca;
+namespace BibliotecaControlador
+{
+    public class DaoPersona
+    {
+        static List<Persona> listado;
+        public DaoPersona()
+        {
+            if (listado==null)
+            {
+                listado = new List<Persona>();
+            }
+            
+        }
+        public bool Grabar(Persona p) {
+            listado.Add(p);
+            return true;
+        }
+    }
+}
